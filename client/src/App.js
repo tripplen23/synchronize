de0d8480b65ff4d664070d9cd2f1c111f6e0535b
tripplen63 <tripplen63@gmail.com>
2024-01-19 17:29:02 +0200

    Adding locomotive-scroll and almost done home section

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import { dark } from "./styles/Theme";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+import Home from "./sections/Home";
 
 function App() {
+  const containerRef = useRef(null);
   return (
     <>
       <GlobalStyles />
 
       <ThemeProvider theme={dark}>
-        <main class="App">App File</main>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            <Home />
+          </main>
+        </LocomotiveScrollProvider>
       </ThemeProvider>
     </>
   );
